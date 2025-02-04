fb3dc519f89878775c08c9c2515bc73fd8946c73
CavidanMammadov <cavidanbm-bp215@code.edu.az>
2025-02-04 22:36:15 +0400

    Added Slider Create

--- NinicoFinalTask/Areas/Admin/Controllers/SliderController.cs
+++ NinicoFinalTask/Areas/Admin/Controllers/SliderController.cs
@@ -2,7 +2,9 @@
 using Microsoft.EntityFrameworkCore;
 using NinicoFinalTask.DataAcces;
 using NinicoFinalTask.Extensions;
+using NinicoFinalTask.Models;
 using NinicoFinalTask.ViewModel.Slider;
+using System.Security.Cryptography;
 
 namespace NinicoFinalTask.Areas.Admin.Controllers
 {
@@ -11,7 +13,7 @@ namespace NinicoFinalTask.Areas.Admin.Controllers
     {
         public async Task<IActionResult> Index()
         {
-            var data = await _context.Sliders.ToListAsync();
+            var data = await _context.Sliders.Where(x=> x.isDeleted == false).ToListAsync();
             return View(data);
         }
         public async Task<IActionResult> Create()
@@ -30,10 +32,63 @@ namespace NinicoFinalTask.Areas.Admin.Controllers
             if (!vm.File.IsValidSize(1024))
             {
                 ModelState.AddModelError("File", "must be less than 1 mb");
-                return View();
+                return RedirectToAction(nameof(Index));
             }
-            File = await vm.File.UploadAsync(_env.WebRootPath, "imgs", "sliders");
-            return View();
+
+            Slider slider = new Slider
+            {
+                ImageUrl = await vm.File.UploadAsync(_env.WebRootPath, "imgs", "sliders"),
+                Link = vm.Link,
+                Title = vm.Title,
+                SubTitle = vm.SubTitle,
+                StartPrice = vm.StartPrice
+            };
+            await _context.Sliders.AddAsync(slider);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        [HttpGet]
+        public async Task<IActionResult> Update(int? id)
+        {
+            if (!id.HasValue) return BadRequest();
+            var data = await _context.Sliders.Where(x => x.isDeleted == false).FirstOrDefaultAsync(x => x.Id == id);
+            if (data is null) return BadRequest();
+            SliderUpdateVM vm = new();
+            data.Title = vm.Title;
+            data.SubTitle = vm.SubTitle;
+            data.StartPrice = vm.StartPrice;
+            data.Link = vm.Link;
+            return View(vm);
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> Update(int? id, SliderUpdateVM vm)
+        {
+            if (!vm.File.IsValidType("image"))
+                ModelState.AddModelError("File", "must be an image");
+            if (!vm.File.IsValidSize(1024))
+                ModelState.AddModelError("FIle", "must be less than 1 mb");
+            if (!ModelState.IsValid) return View();
+            var data = await _context.Sliders.FindAsync(id);
+            if (data is null) return View();
+            data.Title = vm.Title;
+            data.SubTitle = vm.SubTitle;
+            data.Link = vm.Link;
+            data.StartPrice = vm.StartPrice;
+            data.ImageUrl = await vm.File.UploadAsync(_env.WebRootPath, "imgs", "sliders");
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+
+
+        }
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (!id.HasValue) return BadRequest();
+            var data = await _context.Sliders.FindAsync(id);
+            if (data is null) return BadRequest();
+            data.isDeleted = true;
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- NinicoFinalTask/obj/Debug/net8.0/NinicoFinalTask.AssemblyInfo.cs
+++ NinicoFinalTask/obj/Debug/net8.0/NinicoFinalTask.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NinicoFinalTask")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d408599b2ab5b7e279a14e941fedc40b97150833")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5be802786549bba4e17057b392d0709650e08b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("NinicoFinalTask")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NinicoFinalTask")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
